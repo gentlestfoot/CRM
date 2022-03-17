ee8d1e1ce6551c503d1d61635c8b39daee41a905
Matthew Peters <matthew@matthewpeters.ca>
2022-03-16 22:04:14 -0500

    Added views and actions for customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,18 @@
 class CustomersController < ApplicationController
+  def index
+    @title = "All Customers"
+    @customers = Customer.all
+  end
+
+  def alphabetized
+    @title = "Alphabetized"
+    @customers = Customer.order(:name)
+    render :index
+  end
+
+  def missing_email
+    @title = "Missing Email"
+    @customers = Customer.where("email = ''")
+    render :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "customers#index"
+
+  resources :customers do
+    collection do
+      get "alphabetized"
+      get "missing_email"
+    end
+  end
 end
